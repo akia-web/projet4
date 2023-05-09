e228986e65a6ce0af89cb445ca61592e84c73e09
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2023-05-09 10:23:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -31,17 +31,37 @@ const CreateAccountSchema = new Schema({
 
 const CreateAccount = model("CreateSchema", CreateAccountSchema);
 
+// server.post("/account", async (request, reply) => {
+//   //   console.log(request.body);
+//   const { email, password } = request.body;
+//   try {
+//     let hash = await hash(password, await genSalt(10));
+//     password = await hash(password, salt);
+//   } catch (error) {}
+
+//   const account = new CreateAccount({ email, password });
+//   await account.save();
+//   reply.code(201).send("Recettes enregistrées avec succès !");
+// });
+
+
 server.post("/account", async (request, reply) => {
-  //   console.log(request.body);
   const { email, password } = request.body;
   try {
-    let hash = await hash(password, await genSalt(10));
-    password = await hash(password, salt);
-  } catch (error) {}
-
-  const account = new CreateAccount({ email, password });
-  await account.save();
-  reply.code(201).send("Recettes enregistrées avec succès !");
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(password, salt);
+    const account = new CreateAccount({ email, password: hashedPassword });
+    await account.save();
+    reply.code(201).send("Recettes enregistrées avec succès !");
+  } catch (error) {
+    console.log(error);
+    reply
+      .code(500)
+      .send("Une erreur est survenue lors de la création du compte");
+  }
 });
+
+
+
 
 server.delete("/account", async (request, reply) => {});
