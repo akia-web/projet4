990deddd537419fedcec910fa5852c2659dfa813
charline <croyer@normandiewebschool.fr>
2023-05-09 09:48:54 +0200

    create account

--- main.js
+++ main.js
@@ -3,16 +3,37 @@ import { connect, Schema, model } from "mongoose";
 import fs from "fs";
 
 const server = fastify({ logger: true });
-connect("mongodb://localhost:27017/apiHotwing")
-    .then(() => console.log("connecté"))
-    .catch((erreur) => console.log(erreur));
+connect("mongodb://localhost:27017/account-projet4")
+  .then(() => console.log("connecté"))
+  .catch((erreur) => console.log(erreur));
 
 const start = async () => {
-    try {
-        server.listen({ port: 3000 });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    server.listen({ port: 3000 });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 start();
+
+const CreateAccountSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const CreateAccount = model("CreateSchema", CreateAccountSchema);
+
+server.post("/account", async (request, reply) => {
+  console.log(request.body);
+  const { email, password } = request.body;
+  const account = new CreateAccount({email, password});
+  await account.save();
+  reply.code(201).send("Recettes enregistrées avec succès !");
+});
